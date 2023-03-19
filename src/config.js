d2881726a63b39ac9497509606f30fc2f80676a5
Dean <deaneffi1414@gmail.com>
2023-03-19 10:32:32 +0200

    initial

---
+++ src/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+const app = initializeApp(firebaseConfig);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD40KeWCdMbAKszWAzN95NOVAOSXilS4y4",
+  authDomain: "peeper-346c9.firebaseapp.com",
+  projectId: "peeper-346c9",
+  storageBucket: "peeper-346c9.appspot.com",
+  messagingSenderId: "553069121794",
+  appId: "1:553069121794:web:d23d3653b5bd265da4721b",
+  measurementId: "G-6VGV0DG1SK"
+};
